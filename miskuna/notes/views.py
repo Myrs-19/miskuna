b8d07848ffa31d8542932f7599bd6e54e1513648
Mike <my.rs@mail.ru>
2022-10-03 11:01:35 +0300

    Добавление возможности удалять конктретную задачу в уже добавленную панель

--- miskuna/notes/views.py
+++ miskuna/notes/views.py
@@ -5,7 +5,9 @@ from django.forms import formset_factory
 from .models import *
 from .forms import *
 
-def savePanel(request):
+import re
+
+def save_panel(request):
     for key, item in request.POST.items():
         if key == 'form-0-text':
             p = Panel(title=item)
@@ -16,12 +18,12 @@ def savePanel(request):
 
 
 # post, change
-def changePanel(request):
+def change_panel(request):
 
     tasks = [Task.objects.get(pk=key) for key, value in request.POST.items() if key.isnumeric()]
 
     # получаем панель которую будет изменять
-    panel = Panel.objects.get(pk=request.POST['change'])
+    panel = Panel.objects.get(pk=request.POST['change']) # в request.POST['change'] хранится pk панели
     panel.title=request.POST['title']
     panel.save()
 
@@ -29,6 +31,20 @@ def changePanel(request):
     for task in tasks:
         task.text = request.POST[str(task.pk)] #type(task.pk) - int, ключи словаря request.POST только строки
         task.save()
+
+
+def del_task(pk):
+    Task.objects.get(pk=pk).delete()
+
+
+def get_key_for_del(request):
+    '''
+        возвращает ключ элемента (task's object), который нужно удалить
+        возвращает пустую строку при отсутствии ключа (пользователь выбрал другое действие)
+    '''
+
+    l_key = [key for key in request.POST.keys() if re.search("del-\d", key)]
+    return "".join(l_key) if l_key else ""
 
 
 def index(request):
@@ -41,27 +57,34 @@ def index(request):
     }
     formset = formset_factory(FormTask, extra=2)
 
-    t = ""
-
     if request.method == "POST":
         last_item = list(request.POST.keys())[-1]
-        if 'new_panel' == last_item:
-            # if request
-            savePanel(request)
-        if 'change' == last_item:
-            t = changePanel(request)
-        if 'del' == last_item:
-            Panel.objects.get( id = request.POST[last_item]).delete()
 
         if 'add_field' == last_item:
             for key, item in request.POST.items(): #переименовать item <- задача для программиста, для тебя блять еблан
                 if 'form' in key:
                     data[key] = item
-
             #минус crsf токен
             add = len(request.POST) - 1
             data['form-TOTAL_FORMS'] = str(add)
             formset = formset_factory(FormTask, extra=add) #перегружаем formset для количества add
+
+        if 'new_panel' == last_item:
+            # if request
+            save_panel(request)
+
+        if 'change' == last_item:
+            t = change_panel(request)
+
+        if 'del-panel' == last_item:
+            Panel.objects.get( id = request.POST[last_item]).delete()
+        
+        # возвращает пустую строку если нет объект не найден
+        if get_key_for_del(request):
+            task_item = get_key_for_del(request)
+            del_task(request.POST[ task_item ]) #передаем pk таска который удаляем
+            
+
 
     # form = FormTask()
     content = {
@@ -70,7 +93,6 @@ def index(request):
         'formset' : formset(data),
         'g' : request.GET,
         'p' : request.POST,
-        "t" : t if t else "not"
     }
 
     return render(request, "notes/index.html", content)
